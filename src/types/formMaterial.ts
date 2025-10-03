c72eb2ed669aaa219b8426f195912192aaccb1be
warlleyrocha <warlleyrocha@icloud.com>
2025-10-03 13:22:19 -0300

    refactor: tornar o campo 'código' opcional e ajustar comentários no schema de materiais

--- src/types/formMaterial.ts
+++ src/types/formMaterial.ts
@@ -3,17 +3,15 @@ import { z } from 'zod';
 export const materialSchema = z
   .object({
     name: z.string().min(1, 'Nome do material é obrigatório'),
-    code: z
-      .string()
-      .min(1, 'Código é obrigatório')
-      .max(12, 'Código deve ter no máximo 12 caracteres'),
+    code: z.string().max(12, 'Código deve ter no máximo 12 caracteres').optional(), // Código é opcional
+
     unit: z.enum(['unidade', 'metro'], { message: 'Selecione a unidade do material' }),
     quantity: z.number().min(0.01, 'Quantidade mínima é 0.01'),
   })
   .superRefine((obj, ctx) => {
     if (obj.unit === 'unidade' && !Number.isInteger(obj.quantity)) {
       ctx.addIssue({
-        code: 'custom', // <-- usar string literal "custom" em vez de ZodIssueCode.custom
+        code: 'custom',
         message: 'Quantidade deve ser um número inteiro para unidades',
         path: ['quantity'], // aponta o erro para o campo quantity
       });
@@ -23,19 +21,19 @@ export const materialSchema = z
 export const formSchema = z.object({
   officer: z.object({
     name: z.string().min(1, 'Nome do técnico é obrigatório'),
-    secondName: z.string().optional(), //Secundo técnico opcional
+    secondName: z.string().optional(), // Segundo técnico opcional
     registration: z
       .string()
       .min(1, 'Matrícula do técnico é obrigatória')
       .max(10, 'Matrícula deve ter no máximo 10 caracteres'),
-    secondRegistration: z.string().max(10, 'Matrícula deve ter no máximo 10 caracteres').optional(),
+    secondRegistration: z.string().max(10, 'Matrícula deve ter no máximo 10 caracteres').optional(), // Segunda matrícula opcional
     city: z.string().min(1, 'Cidade é obrigatória'),
     state: z
       .string()
       .length(2, 'Estado deve ter 2 caracteres')
       .transform((val) => val.toUpperCase()),
     street: z.string().min(1, 'Rua é obrigatória'),
-    number: z.string().max(5, 'Número da rua deve ter no máximo 5 caracteres').optional(),
+    number: z.string().max(5, 'Número da rua deve ter no máximo 5 caracteres').optional(), // Numero da rua opcional
     hood: z.string().min(1, 'Bairro é obrigatório'),
     date: z.string().min(1, 'Data é obrigatória'),
     activity: z.string().optional(),
